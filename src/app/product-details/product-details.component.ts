78f5bf13a3819739bf1e942ee8f18de899cef719
Radu-Florian-Sava <radu.sava@stud.ubbcluj.ro>
2022-08-30 20:58:18 +0300

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -13,8 +13,16 @@ export class ProductDetailsComponent implements OnInit {
 
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute){ }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+    ){ }
 
+  addToCart(product: Product){
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+  
   ngOnInit() {
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
